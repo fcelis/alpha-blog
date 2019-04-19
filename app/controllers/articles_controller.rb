d2962bb0c4d8ed049f13b7c73a0ac86fcd38766a
Fernando Celis <fc888.2009@gmail.com>
2019-04-19 02:55:50 +0000

    updates

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,12 +7,12 @@ class ArticlesController < ApplicationController
     render plain: params[:article].inspect
     @article = Article.new(article_params)
     @article.save
-    redirect_to_artcicles_show(@article)
+ #   redirect_to_artcicles_show(@article)
   end
 
   private
   def article_params
-    params.require(:article).permit(:article, :description)
+    params.require(:article).permit(:title, :description)
   end
 
 end
--- db/migrate/20190415030720_create_articles.rb
+++
@@ -1,6 +0,0 @@
-class CreateArticles < ActiveRecord::Migration[5.2]
-  def change
-    create_table :articles do |t|
-    end
-  end
-end
