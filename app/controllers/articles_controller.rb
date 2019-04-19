7b8a44802766c4bb81a3b5ffa4e61579f0bd1f62
Fernando Celis <fcelis@alpine-usa.com>
2019-04-18 20:30:05 -0700

    adding update method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,18 @@ class ArticlesController < ApplicationController
   def new
      @article = Article.new
   end
-
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+      @article = Article.find(params[:id])
+      if @article.update
+        flash[:notice] = "Article was successfully updated"
+        redirect_to articles_path(@article)
+      else
+        render 'edit'
+      end
+  end
   #.........................................................................
   def create
     @article = Article.new(article_params)
