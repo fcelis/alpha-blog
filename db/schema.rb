0e374e02504e332d8ef2c71cfa60b3b6f7615506
Fernando Celis <fc888.2009@gmail.com>
2019-04-25 02:15:55 +0000

    add association between user and articles and add debbuger view

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_24_140238) do
+ActiveRecord::Schema.define(version: 2019_04_25_015134) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
