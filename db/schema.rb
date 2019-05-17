dff065335758e84926af77672a5df71ca492fe8b
Fernando Celis <fc888.2009@gmail.com>
2019-05-17 15:02:45 +0000

    add admin user functionality and user acesss

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_25_030310) do
+ActiveRecord::Schema.define(version: 2019_05_17_033351) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_04_25_030310) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
