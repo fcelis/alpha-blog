c5ddcf4145fadc83348b5465c0cc5b6d35d7df3e
Fernando Celis <fc888.2009@gmail.com>
2019-06-20 03:43:21 +0000

    Add Article and category association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_145136) do
+ActiveRecord::Schema.define(version: 2019_06_14_221557) do
+
+  create_table "article_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
